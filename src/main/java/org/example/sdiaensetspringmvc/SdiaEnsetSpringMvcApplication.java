c0563b1b6cedd76ee42b6b677458d90abb166bdd
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-03-08 04:34:51 +0000

    Créer un projet Spring Boot avec les dépendances : Spring Web, Spring Data JOA, H2, MySQL, Thymeleaf, Lombock, Spring Security, Spring Validation

---
+++ src/main/java/org/example/sdiaensetspringmvc/SdiaEnsetSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package org.example.sdiaensetspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiaEnsetSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdiaEnsetSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sdiaensetspringmvc/SdiaEnsetSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sdiaensetspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiaEnsetSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
