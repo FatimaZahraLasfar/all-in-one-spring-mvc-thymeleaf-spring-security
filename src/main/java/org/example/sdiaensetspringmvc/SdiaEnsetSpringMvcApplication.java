535cf651b30508ebbb2465869b8cf136358c23c2
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-03-09 01:00:40 +0000

    Désactiver la protection par défaut de spring security

--- src/main/java/org/example/sdiaensetspringmvc/SdiaEnsetSpringMvcApplication.java
+++ src/main/java/org/example/sdiaensetspringmvc/SdiaEnsetSpringMvcApplication.java
@@ -1,13 +1,43 @@
 package org.example.sdiaensetspringmvc;
 
+import org.example.sdiaensetspringmvc.entities.Product;
+import org.example.sdiaensetspringmvc.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SdiaEnsetSpringMvcApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SdiaEnsetSpringMvcApplication.class, args);
     }
+    @Bean
+    public CommandLineRunner start(ProductRepository productRepository){
+        return args -> {
+            productRepository.save(Product.builder()
+                    .name("Computer")
+                    .price(5400)
+                    .quantity(12)
+                    .build());
+            productRepository.save(Product.builder()
+                    .name("Printer")
+                    .price(1200)
+                    .quantity(11)
+                    .build());
+            productRepository.save(Product.builder()
+                    .name("Smart Phone")
+                    .price(12000)
+                    .quantity(33)
+                    .build());
+            productRepository.findAll().forEach(p ->{
+                System.out.println(p.toString());
+            });
+        };
 
+    }
 }
